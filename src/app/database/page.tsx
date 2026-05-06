2f6f25ef9d5863821854417de9dd64a52ebfe1e7
Ivan Belkovskiy <appelupdater@gmail.com>
2026-05-06 23:03:50 +0300

    Fix Some Bugs

--- src/app/database/page.tsx
+++ src/app/database/page.tsx
@@ -1,6 +1,6 @@
 // 'use client';
 // import DataPageWrapper from "@/components/DataPageWrapper/DataPageWrapper";
-import DataTable from "@/components/DataTable/DataTable";
+// import DataTable from "@/components/DataTable/DataTable";
 // import TableSelector from "@/components/TableSelector/TableSelector";
 // import { PrismaClient } from "@/generated/prisma/internal/class";
 // import { prisma } from "@/lib/prisma";
@@ -21,7 +21,7 @@ export default function DatabasePage() {
         <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
             <h1 className="font-700 text-[25px]">Database</h1>
             <hr />
-            <DataTable table={'simple_task_manager__tasks'} />
+            {/* <DataTable table={'simple_task_manager__tasks'} /> */}
             
         </div>
     );
